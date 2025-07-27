fc072c3e4eaf22f87d8c455adb9fd3f3220b0512
tanya bhardwaj <tanyabhardwaj59@gmail.con>
2025-07-27 13:44:16 +0530

    Updated

--- rail-madad-backend-express/routes/adminRoute.js
+++ rail-madad-backend-express/routes/adminRoute.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import { getPendingComplaints, changeComplaintStatus } from '../controllers/adminController.js';
-// import { adminAuth } from '../middlware/adminAuth.js';
+import { adminAuth } from '../middlware/adminAuth.js';
 
 const router = express.Router();
-
-// router.post('/verify-login', adminAuth, verifyLogin);
-router.get('/get-complaints', getPendingComplaints);
+router.get('/get-complaints', adminAuth,getPendingComplaints);
 router.post('/change-status', changeComplaintStatus);
 
 export default router;
